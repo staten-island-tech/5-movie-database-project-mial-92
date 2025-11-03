6f324cc25fb44fadc6565710ef0c84767ecd1230
mial-92 <mialin0170@gmail.com>
2025-11-03 09:40:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import json
 movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
-
+for i in range(0,len(data)):
+        print(data[i]["title"])
+        
 genre_list = ["Drama", 
               "Crime", 
               "War", 
@@ -27,12 +29,12 @@ genre_list = ["Drama",
               "Suspense", 
               "Animated"]
 for i in range(0, len(data)):
-    print(data[i]["title"])
     print(f"These are the genres: {genre_list}")
     genre = input("What genre are you looking for? ")
     year = input("What year, 1961 - 2023 ")
     if genre in data[i]["genres"] and year in str(data[i]["year"]):
         print(data[i]["title"])
+
 
 
 
